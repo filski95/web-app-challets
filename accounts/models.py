f0ca702483630d000747afb4487b4e4790e82257
Filip Skiba <fskibaa@gmail.com>
2022-08-14 18:28:09 +0200

    custom user, admin + creation forms

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,74 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, name, surname, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email or not name or not surname:
+            raise ValueError("Users must have an email address, first name and last name")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            surname=surname,
+            **extra_fields,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, surname, password=None, **extra_fields):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        user = self.create_user(
+            email,
+            password=password,
+            name=name,
+            surname=surname,
+            **extra_fields,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class MyCustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name="email address", max_length=40, unique=True)
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    date_of_birth = models.DateField(null=True, blank=True)  # not required at all
+    city = models.CharField(max_length=25)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = "email"  # email as main username
+
+    REQUIRED_FIELDS = ["name", "surname"]  # admin createsuperuser
+
+    def __str__(self) -> str:
+        return f"User: {self.name} {self.surname}: {self.email}"
+
+    # @property
+    # def is_staff(self):
+    #     return self.is_admin
+
+    @property
+    def full_name(self):
+        return f"{self.name} {self.surname}"
--- core_project/settings.py
+++ core_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS: list[str] = []
+ALLOWED_HOSTS: list[str] = ["0.0.0.0"]
 
 
 # Application definition
@@ -133,3 +133,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.MyCustomUser"
